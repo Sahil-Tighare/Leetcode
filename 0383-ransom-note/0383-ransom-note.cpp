5d3315d08368f26e461108e47566143e209d167f
Sahil-Tighare <sahil110507t@gmail.com>
2026-09-01 18:58:12 +0530

    Time: 3 ms (52.07%), Space: 11.6 MB (98.14%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        for(int i=0;i<ransomNote.size();i++){
+            bool found = false;
+            for(int j=0;j<magazine.size();j++){
+                if(ransomNote[i] == magazine[j]){
+                    magazine.erase(j, 1);
+                    found = true;
+                    break;
+                }
+            }if(found == false){
+                return false;
+            }
+        }
+        return true;
+    }
+};
